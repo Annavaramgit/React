da68eb3bb5590eb771bdac87761ecc4f337871a8
Annavaramgit <satyanarayanaraavi@123@gmail.com>
2024-11-28 14:24:38 +0530

    changes made

--- myfirst/src/About.js
+++ myfirst/src/About.js
@@ -1,10 +1,17 @@
+import { useState } from "react"
 
 
 const About = () => {
- 
+
+  const [user, setUser] = useState(10)
+
   return (
-    <div>
-        <h1>About page...</h1>
+    <div className="about-container">
+      <div className="child1">Header</div>
+      <div className="child2">nav</div>
+      <div className="child3">body</div>
+      <div className="child4">aside</div>
+      <div className="child5">footer</div>
     </div>
   )
 }
--- myfirst/src/Contact.js
+++ myfirst/src/Contact.js
@@ -1,12 +1,27 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const Contact = () => {
  
-
+let x=2;
+console.log("result",x**x--);
+console.log("x is",x)
   return (
     <div>
-      <h1>Contact page......</h1>
+      
+      <div className='parent'>
+      
+      <FontAwesomeIcon icon={faEnvelope} />
+      <button >
+        <FontAwesomeIcon icon={faFacebook} style={{ marginRight: '8px' }} />
+        Facebook
+      </button>
+        <div className='child'>box</div>
+        {/* <div className='child'>box</div> */}
+      </div>
+
     </div>
   )
 }
